2dd56e7b4cd832ffe18d8bd19b2ef9deaa68436e
Siyuan Wang <12012324@mail.sustech.edu.cn>
2024-11-27 21:05:42 +0800

    add flat_a1 config

--- legged_gym/envs/a1/a1_flat_config.py
+++ legged_gym/envs/a1/a1_flat_config.py
@@ -27,29 +27,28 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
-#
-# Modified by SiyuanWang, 2024
 
-from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
-from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .base.legged_robot import LeggedRobot
-from .anymal_c.anymal import Anymal
-from .anymal_c.mixed_terrains.anymal_c_rough_config import AnymalCRoughCfg, AnymalCRoughCfgPPO
-from .anymal_c.flat.anymal_c_flat_config import AnymalCFlatCfg, AnymalCFlatCfgPPO
-from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
-from .cassie.cassie import Cassie
-from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
-from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .a1.a1_flat_config import A1FlatCfg, A1FlatCfgPPO
+from legged_gym.envs import A1RoughCfg, A1RoughCfgPPO
 
+class A1FlatCfg( A1RoughCfg ):
+    class env( A1RoughCfg.env ):
+        num_observations = 48
+  
+    class terrain( A1RoughCfg.terrain ):
+        mesh_type = 'plane'
+        measure_heights = False
+  
+class A1FlatCfgPPO( A1RoughCfgPPO ):
+    class policy( A1RoughCfgPPO.policy ):
+        actor_hidden_dims = [128, 64, 32]
+        critic_hidden_dims = [128, 64, 32]
+        activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
 
-import os
+    class algorithm( A1RoughCfgPPO.algorithm):
+        entropy_coef = 0.01
 
-from legged_gym.utils.task_registry import task_registry
-
-task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
-task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
-task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
-task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
-task_registry.register( "a1_flat", LeggedRobot, A1FlatCfg(), A1FlatCfgPPO() )
-task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+    class runner ( A1RoughCfgPPO.runner):
+        run_name = ''
+        experiment_name = 'flat_a1'
+        load_run = -1
+        max_iterations = 300
