8fe1bd069c403864fef8d68cafb759661d45a043
Siyuan Wang <12012324@mail.sustech.edu.cn>
2025-05-08 17:29:24 +0800

    simple fix

--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -55,7 +55,7 @@ def play(args):
     obs = env.get_observations()
     # load policy
     train_cfg.runner.resume = True
-    ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args, train_cfg=train_cfg)
+    ppo_runner, train_cfg, _ = task_registry.make_alg_runner(env=env, name=args.task, args=args, train_cfg=train_cfg)
     policy = ppo_runner.get_inference_policy(device=env.device)
     
     # export policy as a jit module (used to run it from C++)
